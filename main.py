4a94ae9e579d151ae7a91a2cf7911997a1b19f50
Ryan Locascio <63062893+RyanL2020@users.noreply.github.com>
2022-01-31 16:53:11 +0000

    equations mapped out

--- main.py
+++ main.py
@@ -6,14 +6,19 @@
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 #Write your code below this line 👇
-
+# divide total_bill_number / guest_split_number * 1.12
+# round guest_split_number 2 decimal spaces
 print("Welcome to the tip calculator!")
 
-total_bill = input("What was the total bill?")
-total_bill_number = float("total_bill")
+total_bill = input("How much is the bill?")
+total_bill_number = int(total_bill)
 
 percent_tip = input("How much tip would you like to give? 10, 12, or 15?")
-percent_tip_number = int("percent_tip")
+percent_tip_number = int(percent_tip)
 
 guest_split = input("How many people to split the bill?")
-guest_split_number = int("guest_split")
+guest_split_number = int(guest_split)
+
+result = (total_bill_number / guest_split_number) * 1.12
+result_rounded = round(result, 2)
+print(f"Everyone pays: {result_rounded}")
